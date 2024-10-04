811815bfcca194dad287205138fd52041635d94b
nitis07 <Nitishrai7280@gmail.com>
2024-10-04 22:55:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# counter-using-js
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nitis07/counter-using-js)
---
+++ script.js
@@ -0,0 +1,4 @@
+const label = document.getElementById('labl');
+const increase = document.querySelector('#increase');
+const reset = document.querySelector('#reset');
+const decrease = document.querySelector('#decrease');
