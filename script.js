5f0a0628aef42e2fc323b6c36f6792237a292423
nitis07 <nitishrai7280@gmail.com>
2024-10-04 23:05:43 +0530

    half project done remaining i will do tommorow

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const label = document.getElementById('labl');
 const increase = document.querySelector('#increase');
 const reset = document.querySelector('#reset');
 const decrease = document.querySelector('#decrease');
+
+let num = 0
+
+increase.addEventListener("click",()=>{
+ num ++;
+ label.innerHTML = `${num}`
+});
