f873a3a69d9be2745f54b3ab7400e3095370510d
nitis07 <nitishrai7280@gmail.com>
2024-10-05 20:27:55 +0530

    done

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ const increase = document.querySelector('#increase');
 const reset = document.querySelector('#reset');
 const decrease = document.querySelector('#decrease');
 
-let num = 0
+let num = 0;
+ let playGame = false;
 
-increase.addEventListener("click",()=>{
- num ++;
- label.innerHTML = `${num}`
+  increase.addEventListener("click",()=>{
+  num ++;
+  label.innerHTML = `${num}`
+playGame = true
+
 });
+  decrease.addEventListener("click",()=>{
+   if (num>0) {
+    num--;
+    
+   }
+    label.innerHTML = `${num}`
+    playGame = true
+
+   });
+   reset.addEventListener("click",()=>{
+    num =0;
+    label.innerHTML = `${num}`
+    playGame = false
+
+   });
